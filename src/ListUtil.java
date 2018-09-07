f65d26e6bb9614e1a31cc1f7aaa402b54f79a36e
Kornphon Noiprasert <konphon22@hotmail.com>
2018-09-07 18:44:11 +0700

    Finish problem2<Binary Search>

--- src/ListUtil.java
+++ src/ListUtil.java
@@ -2,29 +2,63 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * ListUtil count the number of distinct elements in a list.
+ * ListUtil count the number of distinct elements in a list and use a binary
+ * search to find index in.
  * 
  * @author Kornphon Noiprasert
  */
 public class ListUtil {
 	/**
-     * Count the number of distinct elements in a list.
-     * The list may be empty but not null.
-     *
-     * @param list a list of elements
-     * @return the number of distinct elements in list
-     */
+	 * Count the number of distinct elements in a list. The list may be empty but
+	 * not null.
+	 *
+	 * @param list a list of elements
+	 * @return the number of distinct elements in list
+	 * @throws if the value in list is null.
+	 */
 	public static int countUnique(List<?> list) {
-		if (list == null) throw new NullPointerException();
+		if (list == null)
+			throw new NullPointerException();
 		int number = 0;
 		List<Object> copyList = new ArrayList<>();
 		copyList.addAll(list);
-		while(copyList.size() != 0) {
+		while (copyList.size() != 0) {
 			Object element = copyList.get(0);
 			copyList.remove(0);
-			if(copyList.indexOf(element) == -1) 
+			if (copyList.indexOf(element) == -1)
 				number++;
 		}
 		return number;
+	}
+
+	/**
+	 * Searches for an element in an array, where the array contents
+	 * are already sorted.
+	 * 
+	 * @param array - contents are already sorted.
+	 * @param element - the value to be searched for.
+	 * @return index of the matching element or -1 if the search element is not in
+	 *         the array.
+	 * @throws if the value to find (element) is null.
+	 */
+	public static <T extends Comparable<? super T>> int binarySearch(T[] array, T element) {
+		if (element == null)
+			throw new IllegalArgumentException("Search element cannot be null.");
+		if (array.length == 0)
+			return -1;
+		int lowerBound = 0;
+		int upperBound = array.length - 1;
+		int medium;
+		do {
+			medium = (lowerBound + upperBound) / 2;
+			int numberStatus = element.compareTo(array[medium]);
+			if (numberStatus == 0)
+				return medium;
+			if (numberStatus >= 1)
+				lowerBound = medium + 1;
+			else
+				upperBound = medium - 1;
+		} while (lowerBound <= upperBound);
+		return -1;
 	}
 }
--- test/ListUtilTest.java
+++ test/ListUtilTest.java
@@ -19,51 +19,115 @@ public class ListUtilTest {
 	 * 
 	 * @param elements - many elements to be in list.
 	 * @return a list with many elements in.
-	 * */
-	private List<?> makeList(Object ... elements){
+	 */
+	private List<?> makeList(Object... elements) {
 		return java.util.Arrays.asList(elements);
 	}
-	
+
 	@Test
 	public void testEmptyList() {
 		List<?> list = new ArrayList<>();
-		assertEquals(0, ListUtil.countUnique(list),TOL);
+		assertEquals(0, ListUtil.countUnique(list), TOL);
 	}
-	
+
 	@Test
 	public void testListSizeOne() {
-		assertEquals(1, ListUtil.countUnique(makeList("one")),TOL);
+		assertEquals(1, ListUtil.countUnique(makeList("one")), TOL);
 	}
-	
+
 	@Test
 	public void testListSizeOneManyOrders() {
-		assertEquals(1, ListUtil.countUnique(makeList("one","one","one","one","one","one")),TOL);
+		assertEquals(1, ListUtil.countUnique(makeList("one", "one", "one", "one", "one", "one")), TOL);
 	}
-	
+
 	@Test
 	public void testListOfTwoItemsManyOrders() {
-		List<?> list = makeList(1,2);
-		assertEquals(2, ListUtil.countUnique(list),TOL);
-		list = makeList(1,2,1,2,1,2,1,2,1,2,2,2,2,2,1,1,1);
-		assertEquals(2,ListUtil.countUnique(list),TOL);
+		List<?> list = makeList(1, 2);
+		assertEquals(2, ListUtil.countUnique(list), TOL);
+		list = makeList(1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 2, 2, 2, 2, 1, 1, 1);
+		assertEquals(2, ListUtil.countUnique(list), TOL);
 	}
-	
-	@Test(expected=java.lang.NullPointerException.class)
+
+	@Test(expected = java.lang.NullPointerException.class)
 	public void testNullList() {
 		ListUtil.countUnique(makeList(null));
 	}
-	
+
 	@Test
 	public void testListOfMultipleSize() {
-		List<?> list = makeList(1,2,3,4,5,5,5,3,2,1,1,2,6);
-		assertEquals(6,ListUtil.countUnique(list),TOL);
-		
-	}
-	
-	@Test
-	public void testListOfMultipleType() {
-		List<?> list = makeList('1',"Hello",222,222,"Hello","Hello",222,4);
-		assertEquals(4,ListUtil.countUnique(list),TOL);
+		List<?> list = makeList(1, 2, 3, 4, 5, 5, 5, 3, 2, 1, 1, 2, 6);
+		assertEquals(6, ListUtil.countUnique(list), TOL);
+
 	}
 
+	@Test
+	public void testListOfMultipleType() {
+		List<?> list = makeList('1', "Hello", 222, 222, "Hello", "Hello", 222, 4);
+		assertEquals(4, ListUtil.countUnique(list), TOL);
+	}
+
+	@Test
+	public void testBinarySearchOneElement() {
+		Integer[] testInt = new Integer[] { 1 };
+		assertEquals(0, ListUtil.binarySearch(testInt, 1));
+		assertEquals(-1, ListUtil.binarySearch(testInt, 5));
+		String[] testString = new String[] { "Hello" };
+		assertEquals(0, ListUtil.binarySearch(testString, "Hello"));
+		assertEquals(-1, ListUtil.binarySearch(testString, "Hi"));
+	}
+
+	@Test
+	public void testBinarySearchEvenSize() {
+		Integer[] testInt = new Integer[] { 1, 2, 3, 4, 5, 6 };
+		assertEquals(0, ListUtil.binarySearch(testInt, 1));
+		assertEquals(1, ListUtil.binarySearch(testInt, 2));
+		assertEquals(2, ListUtil.binarySearch(testInt, 3));
+		assertEquals(3, ListUtil.binarySearch(testInt, 4));
+		assertEquals(4, ListUtil.binarySearch(testInt, 5));
+		assertEquals(5, ListUtil.binarySearch(testInt, 6));
+		assertEquals(-1, ListUtil.binarySearch(testInt, 7));
+		String[] testString = new String[] { "Ant", "Banana", "Coconut", "Octopus", "Peanut", "Zebra" };
+		assertEquals(0, ListUtil.binarySearch(testString, "Ant"));
+		assertEquals(1, ListUtil.binarySearch(testString, "Banana"));
+		assertEquals(2, ListUtil.binarySearch(testString, "Coconut"));
+		assertEquals(3, ListUtil.binarySearch(testString, "Octopus"));
+		assertEquals(4, ListUtil.binarySearch(testString, "Peanut"));
+		assertEquals(5, ListUtil.binarySearch(testString, "Zebra"));
+		assertEquals(-1, ListUtil.binarySearch(testString, "zebra"));
+	}
+
+	@Test
+	public void testBinarySearchOddSize() {
+		Integer[] testInt = new Integer[] { 1, 2, 3, 4, 5 };
+		assertEquals(0, ListUtil.binarySearch(testInt, 1));
+		assertEquals(1, ListUtil.binarySearch(testInt, 2));
+		assertEquals(2, ListUtil.binarySearch(testInt, 3));
+		assertEquals(3, ListUtil.binarySearch(testInt, 4));
+		assertEquals(4, ListUtil.binarySearch(testInt, 5));
+		assertEquals(-1, ListUtil.binarySearch(testInt, 7));
+		String[] testString = new String[] { "Ant", "Banana", "Coconut", "Octopus", "Zebra" };
+		assertEquals(0, ListUtil.binarySearch(testString, "Ant"));
+		assertEquals(1, ListUtil.binarySearch(testString, "Banana"));
+		assertEquals(2, ListUtil.binarySearch(testString, "Coconut"));
+		assertEquals(3, ListUtil.binarySearch(testString, "Octopus"));
+		assertEquals(4, ListUtil.binarySearch(testString, "Zebra"));
+		assertEquals(-1, ListUtil.binarySearch(testString, "zebra"));
+	}
+
+	@Test
+	public void testBinarySearchDuplicate() {
+		Integer[] testInt = new Integer[] { 1, 3, 3, 4, 4, 4, 5 };
+		assertEquals(1, ListUtil.binarySearch(testInt, 3));
+		assertEquals(3, ListUtil.binarySearch(testInt, 4));
+		Integer[] testIntTwo = new Integer[] { 1, 2, 2, 2, 2, 5, 5, 5 };
+		assertEquals(5, ListUtil.binarySearch(testIntTwo, 5));
+	}
+	
+	@Test(expected = java.lang.IllegalArgumentException.class)
+	public void testBinarySearchNullElement() {
+		Integer[] testInt = new Integer[] { 0 };
+		ListUtil.binarySearch(testInt, null);
+	}
+	
+	
 }
